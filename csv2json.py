936e110abc91d1781d8cba8f76453159d9bdea24
Mike Conlon <mconlon@ufl.edu>
2021-03-14 17:00:38 -0400

    improve org naming and local

--- csv2json.py
+++ csv2json.py
@@ -9,6 +9,7 @@ json_file = stdout
 reader = csv.DictReader(csv_file, delimiter='\t')
 json_data = list()
 for row in reader:
-    new_row = { k.replace(" ", "_"):v for (k,v) in row.items()}
-    json_data.append(new_row)
+    new_row = { k.replace(" ", "_"):v for (k,v) in row.items()} # non-blank key values
+    final_row = { k:v for (k,v) in new_row.items() if v != ""}          # empty strings are removed
+    json_data.append(final_row)
 json_file.write(json.dumps(json_data))
--- organization/org-local.sh
+++ organization/org-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Convert local tsv data to JSON"
-python3 ./csv2json.py <$1.tsv $1.json
+python3 ../csv2json.py <$1.tsv >$1.json
 echo "Convert to Raw RDF"
 java -jar ../../JSON2RDF/target/json2rdf-jar-with-dependencies.jar http://my.org# <$1.json | riot --formatted=TURTLE >$1-raw.ttl
 echo "Convert to ${2-new} VIVO RDF"
